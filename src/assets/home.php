808ecbd29f2293cf37f762e44dcefbcf65f5017f
phantrungnghi <trungnghi202@gmail.com>
2024-03-23 16:27:13 +0700

    16:27

---
+++ src/assets/home.php
@@ -0,0 +1,73 @@
+<?php
+/*
+ Template Name: Home
+ */
+?>
+<?php get_header(); ?>
+
+
+<div class="h-screen w-full relative z-0 px-3">
+    <div class="container mx-auto flex items-center relative z-10 h-full font-SamsungOne">
+        <div class="max-w-max size-1/2">
+            <img src="http://auzcapital.webdemo7.com/wp-content/uploads/2024/03/logo.png" alt="logo" class="h-auto w-auto mb-4">
+            <h3 class="lg:text-xl sm:text-sm text-white mb-5 ">A trusted partner in the alternative asset investment
+                landscape,
+                <br> headquartered in Australia, with a focus on the dynamic APAC region.
+            </h3>
+            <div class="flex items-center flex-wrap">
+                <p class="text-white lg:text-xl sm:text-sm flex-auto w-full sm:w-auto">Core strategies:</p>
+                <div class="flex items-center flex-wrap divide-x  divide-slate-50 flex-auto w-full sm:w-auto ">
+                    <a href="#" class="text-white lg:text-xl sm:text-sm pl-0 pr-2 hover:underline sm:px-2">Credit
+                        Markets</a>
+                    <a href="#" class="text-white lg:text-xl sm:text-sm hover:underline px-2">Private Equity</a>
+                    <a href="#" class="text-white lg:text-xl sm:text-sm hover:underline px-2">M&A</a>
+                    <a href="#" class="text-white lg:text-xl sm:text-sm hover:underline px-2">Real Estate</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="slide absolute z-10 bottom-8 left-0  w-full h-auto">
+        <div class="splide" role="group" aria-label="Splide Basic">
+            <div class="splide__track">
+                <ul class="splide__list">
+                    <li class="splide__slide">
+                        <p class="lg:text-xl sm:text-sm text-white ">Private Equity</p>
+                    </li>
+                    <li class="splide__slide">
+                        <p class="lg:text-xl sm:text-sm text-white ">Private Credit</p>
+                    </li>
+                    <li class="splide__slide">
+                        <p class="lg:text-xl sm:text-sm text-white ">Agribusiness</p>
+                    </li>
+                    <li class="splide__slide">
+                        <p class="lg:text-xl sm:text-sm text-white ">Construction & Infrastructures</p>
+                    </li>
+                    <li class="splide__slide">
+                        <p class="lg:text-xl sm:text-sm text-white ">Energy & Resources</p>
+                    </li>
+                    <li class="splide__slide">
+                        <p class="lg:text-xl sm:text-sm text-white ">Information Technology</p>
+                    </li>
+                    <li class="splide__slide">
+                        <p class="lg:text-xl sm:text-sm text-white ">Manufacturing & Trading</p>
+                    </li>
+                    <li class="splide__slide">
+                        <p class="lg:text-xl sm:text-sm text-white ">Transportation & Logistics</p>
+                    </li>
+                    <li class="splide__slide">
+                        <p class="lg:text-xl sm:text-sm text-white ">Medical & Healthcare</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="h-full w-full absolute top-0 -z-10 right-0">
+        <img src="http://auzcapital.webdemo7.com/wp-content/uploads/2024/03/background_home.png" alt="background" class="object-cover w-full h-full">
+    </div>
+</div>
+
+
+
+
+
+<?php get_footer(); ?>
--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,27 @@ window.addEventListener("scroll", (e) => {
         header.classList.remove("bg-primary");
     }
 });
+
+document.querySelector(".btn-eye").addEventListener("click", (e) => {
+    var inputPass = document.querySelector(".form-login #password");
+
+    if (inputPass.getAttribute("type") == "password") {
+        inputPass.setAttribute("type", "text");
+    } else {
+        inputPass.setAttribute("type", "password");
+    }
+});
+
+// tab login & register
+document.querySelector("form .btn-register").addEventListener("click", (e) => {
+    var cardLogin = document.querySelector(".card-login");
+    var cardRegister = document.querySelector(".card-register");
+
+    if (cardLogin.getAttribute("class").includes("hidden")) {
+        cardLogin.classList.remove("hidden");
+        cardRegister.classList.add("hidden");
+    } else {
+        cardLogin.classList.add("hidden");
+        cardRegister.classList.remove("hidden");
+    }
+});
